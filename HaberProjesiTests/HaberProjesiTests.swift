a488c23f327980e521d69bbbbc3437f960f923bd
Melike Sezgin <mel.sez.19@gmail.com>
2025-02-01 21:03:02 +0300

    Initial Commit

---
+++ HaberProjesiTests/HaberProjesiTests.swift
@@ -0,0 +1,17 @@
+//
+//  HaberProjesiTests.swift
+//  HaberProjesiTests
+//
+//  Created by Melike on 1.02.2025.
+//
+
+import Testing
+@testable import HaberProjesi
+
+struct HaberProjesiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
